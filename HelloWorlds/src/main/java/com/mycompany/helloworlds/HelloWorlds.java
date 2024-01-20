79746e187691a457e34af09ebfcb7dab2dd44ab9
Deeann01 <deepika.annapureddy04@gmail.com>
2024-01-19 22:46:42 -0600

    after changes

---
+++ HelloWorlds/src/main/java/com/mycompany/helloworlds/HelloWorlds.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworlds;
+
+/**
+ *
+ * @author S573021
+ */
+public class HelloWorlds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
